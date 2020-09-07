0cf2fd2816e65fcabdc4b1344e4a11dc3dc8f2ff
zhaodaye <932276768@qq.com>
2020-09-07 17:18:52 +0800

    tag1
    修改文件储藏格式

--- pages/happy/happy.js
+++ pages/happy/happy.js
@@ -1,6 +1,6 @@
 // pages/happy/happy.js
+import { wxData1, wxData2 } from '../datas/hd';
 Page({
-
   /**
    * 页面的初始数据
    */
@@ -145,67 +145,13 @@ Page({
   more: function () {
     let index = this.data.happyNum;
     index += 1;
-    if (index > 10 && index <= 100) {
-      let arr = [{
-        name: '兰蔻极光水',
-        index: 0
-      }, {
-        name: '哇哈哈苏打水',
-        index: 1
-      }, {
-        name: 'SK-Ⅱ (75ml)',
-        index: 2
-      }, {
-        name: 'SK-Ⅱ (230ml)',
-        index: 3
-      }, {
-        name: '开始',
-        index: 4
-      }, {
-        name: '现金红包10元',
-        index: 5
-      }, {
-        name: 'YSL口红自选',
-        index: 6
-      }, {
-        name: 'TF-16',
-        index: 7
-      }, {
-        name: '迪奥#999',
-        index: 8
-      }];
+    if (index > 10 && index <= 20) {
+      let arr = wxData1;
       this.setData({
         awards: arr
       })
-    } else if (index > 100) {
-      let arr = [{
-        name: '微信转账1000元',
-        index: 0
-      }, {
-        name: '微信转账2000元',
-        index: 1
-      }, {
-        name: 'SK-Ⅱ (230ml)',
-        index: 2
-      }, {
-        name: '微信转账3000元',
-        index: 3
-      }, {
-        name: '开始',
-        index: 4
-      }, {
-        name: 'YSL口红6支大礼包',
-        index: 5
-      }, {
-        name: 'SK-Two七夕大礼包',
-        index: 6
-      }, {
-        name: 'TF口红自选*3',
-        index: 7
-      }, {
-        name: '千元内眼影、水乳自选一',
-        index: 8
-      }];
+    } else if (index > 20) {
+      let arr = wxData2;
       this.setData({
         awards: arr
       })
